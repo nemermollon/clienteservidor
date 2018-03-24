6b1dcd8b3b1ea80140b8f1c2dcd9b7d4eb7bb924
nemermollon <nemeralbano@hotmail.com>
2018-03-24 18:33:53 -0300

    Update cliente.h

--- cliente/cliente.h
+++ cliente/cliente.h
@@ -1,1 +1,24 @@
+/*
+  Arquivo: cliente.h
+  Detalhes: Declara funções, constantes e variáveis necessárias para o funcionamento do cliente
+*/
 
+#ifndef _CLIENTE_H_
+#define _CLIENTE_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Diretivas de portabilidade... */
+#ifdef _WINDOWS
+#else
+#endif
+
+/* ... */
+
+#ifdef __cplusplus
+}
+#endif /* C++ */
+
+#endif /* _CLIENTE_H_ */
