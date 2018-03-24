c8c226411b2235871b706d4c22c4807ed82c9398
nemermollon <nemeralbano@hotmail.com>
2018-03-24 18:32:20 -0300

    Update servidor.h

--- servidor/servidor.h
+++ servidor/servidor.h
@@ -1,1 +1,24 @@
+/*
+  Arquivo: servidor.h
+  Detalhes: Declara funções, constantes e variáveis necessárias para o funcionamento do servidor
+*/
 
+#ifndef _SERVIDOR_H_
+#define _SERVIDOR_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Diretivas de portabilidade... */
+#ifdef _WINDOWS
+#else
+#endif
+
+/* ... */
+
+#ifdef __cplusplus
+}
+#endif /* C++ */
+
+#endif /* _SERVIDOR_H_ */
